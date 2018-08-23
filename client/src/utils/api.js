8ec2074578e71ebfbad23b4f50105fc85ab46ab1
Timothy <timothy.mathison96@gmail.com>
2018-08-22 20:56:12 -0500

    basic page styling

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -4,7 +4,7 @@ const basePath = process.env.PUBLIC_URL;
 let load = async (resource, send) => {
     let responce = await fetch(`${basePath}/api/${resource}`);
     let result = await responce.json()
-        .then(result => send(result)) //send result back to application
+        .then(result => send(result), () => {}) //send result back to application
         .catch(reason => console.error(`Failed to load response from ${resource}: ${reason}`))
 
     if (responce.status !== 200) { //status other than OK
