27b19dd92eccf6b34404f5b2426cbab71816f2a7
Timothy <timothy.mathison96@gmail.com>
2018-10-14 19:07:37 -0500

    Remove basePath in navigation links causing double url elements

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -5,15 +5,13 @@ import '../styles/header.css'
 
 import {Icon} from './icons';
 
-const basePath = process.env.PUBLIC_URL;
-
 const Header = () => (
     <header>
         <nav>
             <ul className={"inline-nav"}>
-                <li><Link to={`${basePath}/home`}><Icon iconId="home" />Home</Link></li>
-                <li><Link to={`${basePath}/about`}>About</Link></li>
-                <li><Link to={`${basePath}/posts`}>Posts</Link></li>
+                <li><Link to={'/home'}><Icon iconId="home" />Home</Link></li>
+                <li><Link to={'/about'}>About</Link></li>
+                <li><Link to={'/posts'}>Posts</Link></li>
             </ul>
         </nav>
     </header>
