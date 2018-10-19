e6eccddc02afd0cdb3c4aab244d0a1c7d8a7f6a0
Timothy <timothy.mathison96@gmail.com>
2018-10-18 22:20:16 -0500

    Add internal logic to select component

--- client/src/components/select.js
+++ client/src/components/select.js
@@ -3,15 +3,33 @@ import React, {Component}  from 'react';
 class SelectDropdown extends Component {
     constructor(props) {
         super(props);
+        if(!props.id) {
+            throw new Error("id is undefined for select element");
+        }
         this.state = {
-            selected: props.selected
+            selected: props.selected ? props.selected : props.options[0]
+        }
+    }
+
+    shouldComponentUpdate() {
+        return false; //always return false as select re-renders internally
+    }
+
+    handler = () => {
+        let elem = document.getElementById(this.props.id);
+        let selected = this.props.options[elem.selectedIndex];
+        if(selected.key !== this.state.selected.key) {
+            this.props.handler(selected);
+            this.setState({ selected: selected })
         }
     }
 
     render() {
         return(
-            <select>
-                {this.props.options.map((option) => <option>option</option>)}
+            <select id={this.props.id} name={this.props.name} onChange={this.handler}>
+                {this.props.options.map((option) => 
+                    <option key={option.key} value={option.key} defaultValue={this.state.selected.key === option.key}>{option.label}</option>
+                )}
             </select>
         )
     }
