b07b6e45699e28f5f301813b07d3394e69a8fc3f
Timothy <timothy.mathison96@gmail.com>
2018-10-26 16:59:01 -0500

    add skeleton methods in database service

--- services/database.js
+++ services/database.js
@@ -4,4 +4,29 @@ const dynamodb = new AWS.DynamoDB({ apiVersion: '2012-08-10' });
 
 const postsTable = process.env.AWS_POST_TABLE;
 
-module.exports = {};
+const createPost = (post) => {
+    //save a new post database entry and return success
+};
+
+const getPost = (id) => {
+    //return single post entry from database
+};
+
+const getMultiplePosts = (ids) => {
+    //return list of post entries from database
+};
+
+const getUser = (id) => {
+    //return a user entry from database
+};
+
+module.exports = {
+    posts: {
+        create: createPost,
+        get: getPost,
+        getMultiple: getMultiplePosts
+    },
+    users: {
+        get: getUser
+    }
+};
