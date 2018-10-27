bb1b03a0da62b7c70c80edbc0bc90894a596ad8d
Timothy <timothy.mathison96@gmail.com>
2018-10-27 12:06:35 -0500

    Wait to require project until environment has been initialized

--- server.local.js
+++ server.local.js
@@ -1,13 +1,13 @@
 const envLoader = require('dotenv-json');
 
-const server = require('./server.js');
-const port = process.env.PORT || 4000;
-
 // load development environment variables
 if (process.env.NODE_ENV !== 'production') {
     envLoader({ path: ".env.development.json"});
 } else {
     envLoader({ path: ".env.production.local.json"})
 }
+
+const server = require('./server.js');
+const port = process.env.PORT || 4000;
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
