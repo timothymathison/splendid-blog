40513eb080eb8ff039ad24cfe13d997e5799e801
Timothy <timothy.mathison96@gmail.com>
2018-11-29 20:15:59 -0600

    Use bcryptjs instead of bcrypt

--- services/auth.js
+++ services/auth.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const errorUtil = require('../utils/errors');
 const db = require('./database');
