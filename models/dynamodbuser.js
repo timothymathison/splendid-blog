abc57ab3a439e39b0e1c9e250e15e00053492e66
Timothy <timothy.mathison96@gmail.com>
2018-12-03 23:47:39 -0600

    add get properties method to dynamodb user

--- models/dynamodbuser.js
+++ models/dynamodbuser.js
@@ -5,21 +5,21 @@ function DynamoDBUser(u) {
     if(u.ID) {
         this.ID = {"S": u.ID}
     } else {
-        throw new Error('Invalid user, missing "ID" property');
+        throw new Error('Invalid user, missing "ID" property'); // ID is required
     }
 
     if(u.Role) {
         if(u.Role === roles.user || u.Role === roles.admin) {
             this.Role = {"S": u.Role};
         } else {
-            throw new Error('Invalid user, "role" propery is not an allowd value');
+            throw new Error('Invalid user, "role" propery is not an allowd value'); // Role must be one of several values
         }
     } else {
-        throw new Error('Invalid user, missing "Role" property');
+        throw new Error('Invalid user, missing "Role" property'); // Role is required
     }
 
     if(u.HashedPassword) {
-        this.HashedPassword = {"S": u.HashedPassword};
+        this.HashedPassword = {"S": u.HashedPassword}; // HashedPassword is required
     } else {
         throw new Error('invalid user, missing "HashedPassword property');
     }
@@ -38,5 +38,15 @@ function DynamoDBUser(u) {
 };
 
 DynamoDBUser.prototype.getRoles = () => roles;
+DynamoDBUser.prototype.getProperties = function() {
+    return {
+        ID: this.ID.S,
+        Role: this.Role.S,
+        FirstName: this.FirstName ? this.FirstName.S : null,
+        LastName: this.LastName ? this.LastName.S : null,
+        Email: this.Email ? this.LastName.S : null,
+        HashedPassword: this.HashedPassword.S
+    };
+}
 
 module.exports = DynamoDBUser;
