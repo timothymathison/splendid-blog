4371d1642c482bd7c45df67642f03a921ee9e66f
Timothy <timothy.mathison96@gmail.com>
2018-12-08 10:03:08 -0600

    Add chai and mocha, begin on tests

--- test/scripts/generatetoken.js
+++ test/scripts/generatetoken.js
@@ -5,7 +5,7 @@ envLoader({ path: ".env.development.json"});
 const mockDatabase = 'test/services/database';
 process.env['DATABASE'] = mockDatabase; // use mock database
 const testPass = Math.random().toString(36).substring(2); // generate random test password
-process.env['TEST_PASSWORD'] = testPass;
+
 console.log(`Setting database to ${process.env.DATABASE}`);
 const db = require(`../../${mockDatabase}`);
 
