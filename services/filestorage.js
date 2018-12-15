b8740f4354761500c7440fa9320cf927f4179d5d
Timothy <timothy.mathison96@gmail.com>
2018-12-15 16:59:19 -0600

    implement getFile method

--- services/filestorage.js
+++ services/filestorage.js
@@ -6,11 +6,23 @@ const s3 = new AWS.S3({ apiVersion: '2006-03-01' });
 
 const bucketName = process.env.AWS_S3_BUCKET || 'splendidblog';
 
-const getFile = (path) => {
-
+const getFile = (path) => { // TODO: test with raw image files
+    return new Promise((resolve, reject) => {
+        let params = {
+            Bucket: bucketName,
+            Key: path
+        };
+        s3.getObject(params, (err, data) => {
+            if(err) {
+                reject(err);
+            } else {
+                resolve(data.Body); // must call .toString('utf-8') for text files
+            }
+        });
+    });
 };
 
-const saveFile = (path, data) => {
+const saveFile = (path, data) => { // TODO: test with raw image files
     return new Promise((resolve, reject) => {
         if(!path.match(new RegExp('^[A-Za-z0-9].*'))) {
             reject('Invalid file path specified'); //only allow numbers and letters to start path
