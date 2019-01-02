a3cad372e995d29d6671d33e764d50c5ed3a7649
Timothy <timothy.mathison96@gmail.com>
2019-01-01 18:08:43 -0600

    import and run server directly so server can be stopped after test

--- test/integration.js
+++ test/integration.js
@@ -1,20 +1,28 @@
 const newman = require('newman');
+const envLoader = require('dotenv-json');
+envLoader({ path: ".env.development.json"}); // load development environment
 
+console.log('Test API Routes, running Postman collection...');
+
+// setup test environment
 process.env['NODE_ENV'] = 'test';
 process.env['DATABASE'] = 'test/services/database'; // use mock database
 console.log(`Setting database to ${process.env.DATABASE}`);
-require('../server.local'); // start local server
 
+const server = require('../server');
 const port = process.env.PORT || 4000;
+const listener = server.listen(port, () => console.log(`Listening on port ${port}`)); // start server locally
+
 const address = `http://localhost:${port}`;
 
 newman.run({
     environment: {'host': address}, // TODO change 'host' to 'address'
     collection: require('./postman.json'),
     reporters: 'cli'
-}, function (err) {
+}, (err) => {
 	if (err) { throw err; }
     console.log('Postman collection run complete!');
 });
 
-// TODO: stop server
+// stop server
+listener.close();
