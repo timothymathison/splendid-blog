840ef2dc351a93dc0682d0c3316a0f74cd738531
Timothy <timothy.mathison96@gmail.com>
2019-03-03 17:21:04 -0600

    create beginning skeleton for post controller

---
+++ controllers/posts.js
@@ -0,0 +1,25 @@
+
+const db = require(`../${process.env.DATABASE || 'services/database'}`);
+const fileStorage = require(`../${process.env.FILE_STORAGE || 'services/filestorage'}`);
+const sendError = require('../utils/errors');
+
+// create and save a post
+const createPost = async req => {
+
+};
+
+// edit an existing post
+const editPost = async req => {
+
+};
+
+// get a complete post
+const getPost = async req => {
+
+};
+
+// get a list containing the metadata for multiple posts
+const getPreviews = async req => {
+
+};
+
--- routes/posts.js
+++ routes/posts.js
@@ -2,11 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const router = express.Router();
 
-const authService = require('../controllers/auth')
+const authController = require('../controllers/auth');
+const roles = require('../models/DynamoDBUser').prototype.getRoles();
 
-router.use(bodyParser.json());
-
-router.post('/create', authService.require('admin'), (req, res) => {
+router.post('/create', authController.require(roles.admin), (req, res) => {
     res.send(req.body);
 });
 
