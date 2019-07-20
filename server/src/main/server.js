046133b3127fcbe48c6155f9d45acd30caaa98e7
Timothy <timothy.mathison96@gmail.com>
2019-07-20 12:07:12 -0500

    simplify setup

--- server/src/main/server.js
+++ server/src/main/server.js
@@ -6,15 +6,13 @@ const api = require('./routes/api')
 
 server.use('/api', api);
 
-if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'test') {
-    // Serve any static files
-    server.use(express.static(path.join(__dirname, 'client/build')));
+// Serve any static files
+server.use(express.static(path.join(__dirname, 'client/build')));
 
-    // Handle React routing, return all requests to React app
-    server.get('*', function(req, res) {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-    });
-}
+// Handle React routing, return all requests to React app
+server.get('*', function(_, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 //export so lambda handler can use
 module.exports = server;
--- server/src/main/server.local.js
+++ server/src/main/server.local.js
@@ -2,10 +2,11 @@ const envLoader = require('dotenv-json');
 
 // load development environment variables
 if (process.env.NODE_ENV !== 'production') {
-    envLoader({ path: "server/config/.env.development.json"});
+    envLoader({ path: "server/config/.env.development.json" });
 } else {
-    envLoader({ path: "server/config/.env.production.local.json"})
+    envLoader({ path: "server/config/.env.production.json" });
 }
+process.env['AWS_S3_BUCKET'] = 'us-east-2'; // can't be included in files above for deployment reasons
 
 const server = require('./server.js');
 const port = process.env.PORT || 4000;
